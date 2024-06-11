954d1e601e6bc2a5e4bb897d5d90b68bf62ff62f
nephrite <oneabove232@gmail.com>
2024-06-11 10:43:50 +0800

    feat: added zwift logo favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import React from "react";
 import Image from "next/image";
 const lato = Lato({ subsets: ["latin"], weight: "400" });
-import Button from "@/components/button/Button";
 import Link from "next/link";
 import SideBar from "@/components/sideBar/SideBar";
 
@@ -15,7 +14,7 @@ export const metadata: Metadata = {
     url: "https://staging.codebility.tech/", // NOTE: is this correct?
     name: "Codibility",
   },
-  keywords: ["Zwift Tech", "ZwiftTech", "Codibility"],
+  keywords: ["Zwift Tech", "ZwiftTech", "Codibility"],  
 };
 
 export default function RootLayout({
@@ -27,7 +26,9 @@ export default function RootLayout({
     <html lang="en" className="bg-gray-100">
       <body className={lato.className}>
         <nav className="w-full pt-6 px-2 sm:px-8 md:px-12 lg:px-16 flex flex-row gap-x-4 justify-between items-center">
-          <Image src={"./logo.svg"} width={100} height={56} alt="ZwiftTech logo" />
+          <Link href={"/"}>
+            <Image src={"./logo.svg"} width={100} height={56} alt="ZwiftTech logo" />
+          </Link>
           <div className="hidden sm:flex flex-row items-center gap-x-6 md:gap-x-10">
             <div className="flex flex-row gap-x-3 md:gap-x-6 font-semibold">
               <Link
